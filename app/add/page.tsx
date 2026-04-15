50df1a16705a2684e1fba120d6d3bd6a0f30b4a0
Govinda Hosein <govinda.hosein@gmail.com>
2026-04-14 23:35:01 -0400

    Add add page

---
+++ app/add/page.tsx
@@ -0,0 +1,8 @@
+export default function Add() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold mb-8">Add a Card</h1>
+      {/* Form to add a card will go here */}
+    </main>
+  );
+}
